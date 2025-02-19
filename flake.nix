e940ce9b92480da12943abb1d7542d74058a5a5f
Ole Tytlandsvik <ole.tytlandsvik@gmail.com>
2025-02-19 12:38:39 +0100

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          nativeBuildInputs = [
+            emscripten
+            cmake
+            python3
+            wabt
+          ];
+          EM_CACHE = "/home/ole/.emscripten_cache";
+        };
+      }
+    );
+}
